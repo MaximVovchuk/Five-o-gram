5aff02f512bfd49ad81b1765d92735dc8d5bd3a6
Maxim Vovchuk <Maxim.Vovchuk@fivesysdev.com>
2022-12-20 16:49:29 +0200

    -Todo

--- src/main/java/com/fivesysdev/Fiveogram/FiveOGramApplication.java
+++ src/main/java/com/fivesysdev/Fiveogram/FiveOGramApplication.java
@@ -15,7 +15,6 @@ public class FiveOGramApplication {
         SpringApplication.run(FiveOGramApplication.class, args);
 
     }
-//TODO mark notification reccom by hashtag, subs on hashtag
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
