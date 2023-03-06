692fcb98629e4640a7a0e58e2c217c411365ca60
Maxim Vovchuk <Maxim.Vovchuk@fivesysdev.com>
2023-03-06 15:08:38 +0200

    code review changes

--- src/main/java/com/fivesysdev/Fiveogram/serviceInterfaces/MessageService.java
+++ src/main/java/com/fivesysdev/Fiveogram/serviceInterfaces/MessageService.java
@@ -1,9 +1,6 @@
 package com.fivesysdev.Fiveogram.serviceInterfaces;
 
-import com.fivesysdev.Fiveogram.models.ChatRoom;
 import com.fivesysdev.Fiveogram.models.MessageModel;
-
-import java.util.List;
 
 public interface MessageService {
     void save(MessageModel model);
